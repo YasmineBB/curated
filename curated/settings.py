44607950fb4fb35b1db2e9e9c6c8e74e1c9599fb
YasmineBB <y.bourhabi@gmail.com>
2024-03-03 13:14:09 +0000

    Added products app and Category, Product and Artist models

--- curated/settings.py
+++ curated/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'artists',
 ]
 
 MIDDLEWARE = [
--- products/models.py
+++ products/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Artist(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    location = models.CharField(max_length=254)
+    bio = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    insta_url = models.CharField(max_length=50, null=True, blank=True)
+    x_url = models.CharField(max_length=50, null=True, blank=True)
+    facebook_url = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    artist = models.ForeignKey('Artist', max_length=254, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
