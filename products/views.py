e87be1d3c00ba4c1a1c6174505f40532f8f146d2
YasmineBB <y.bourhabi@gmail.com>
2024-03-06 09:22:08 +0000

    Added sorting functionality

--- products/views.py
+++ products/views.py
@@ -10,8 +10,23 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lowername'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,9 +36,12 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(category__name__icontains=query) | Q(artist__name__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
