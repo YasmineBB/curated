89a2f3bf784b05449718096d2b857242de2beff9
YasmineBB <y.bourhabi@gmail.com>
2024-03-06 14:32:05 +0000

    Added artists views and templates

--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from products.models import Artist
 
 # Create your views here.
+
+def all_artists(request):
+    """ A view to display all artists """
+
+    artists = Artist.objects.all()
+
+    context = {
+        'artists': artists,
+    }
+
+    return render(request, 'artists/artists.html')
