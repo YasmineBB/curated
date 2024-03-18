8c24af7a836bf418f4897e2693414dbc57959753
YasmineBB <y.bourhabi@gmail.com>
2024-03-18 18:13:40 +0000

    Added basic toast templates

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 
+from products.models import Product
 
 def view_basket(request):
     """ A view that renders the shopping basket contents page """
@@ -8,23 +10,27 @@ def view_basket(request):
 
 
 def add_to_basket(request, item_id):
-    """ Add a quantity of the specified product to the shopping basket """
+    """ A view that adds a specified product to the shopping basket """
 
+
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'Added {product.name} to your basket')
     else:
         basket[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your basket')
         
     request.session['basket'] = basket
     return redirect(redirect_url)
 
 
 def remove_from_basket(request, item_id):
-    """Remove the item from the shopping basket"""
+    """A view that removes a specified item from the shopping basket"""
 
     basket = request.session.get("basket", {})
 
