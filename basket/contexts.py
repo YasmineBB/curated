64393f8d9b50ca55bbbef0d20a1682f75b80f711
YasmineBB <y.bourhabi@gmail.com>
2024-03-18 12:58:08 +0000

    Updated context processor and basket view

--- basket/contexts.py
+++ basket/contexts.py
@@ -9,6 +9,15 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        basket_items.append({
+            'item_id': item_id,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -22,7 +31,7 @@ def basket_contents(request):
     context = {
         'basket_items': basket_items,
         'total': total,
-        'product_count': product_count,
+        # 'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
--- basket/views.py
+++ basket/views.py
@@ -15,9 +15,26 @@ def add_to_basket(request, item_id):
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
-        basket[item_id] += quantity
-    else:
         basket[item_id] = quantity
-
+        
     request.session['basket'] = basket
+    print(request.session['basket'])
     return redirect(redirect_url)
+
+
+# def add_to_basket(request, item_id):
+#     """ Add a quantity of the specified product to the shopping basket """
+
+#     quantity = int(request.POST.get('quantity'))
+#     redirect_url = request.POST.get('redirect_url')
+#     basket = request.session.get('basket', {})
+
+#     if item_id in list(basket.keys()):
+#         basket[item_id] = product.price
+#     else:
+#         request.session['basket'] = basket
+#         return redirect(redirect_url)
+        
+
+#     request.session['basket'] = basket
+#     return redirect(redirect_url)
