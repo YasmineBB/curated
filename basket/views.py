822e5e74c729929477f0edb629caedd78f1d9b05
YasmineBB <y.bourhabi@gmail.com>
2024-03-19 17:56:25 +0000

    Updated toast styling, messaging for removing items and added shopping basket preview

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -13,7 +13,7 @@ def add_to_basket(request, item_id):
     """ A view that adds a specified product to the shopping basket """
 
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -33,9 +33,11 @@ def remove_from_basket(request, item_id):
     """A view that removes a specified item from the shopping basket"""
 
     basket = request.session.get("basket", {})
+    product = get_object_or_404(Product, pk=item_id)
 
     if basket[item_id]:
         basket.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your basket')
 
         request.session['basket'] = basket 
         return HttpResponse(status=200)
