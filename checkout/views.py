682830a627c518449a5265548c58f4f4b2acf057
YasmineBB <y.bourhabi@gmail.com>
2024-03-20 13:40:06 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OdyQhDyRuX5ADdDCL6cnhUWQByFjbmtnx82jOIPi71865qLawuUB1NBWjtbgSGRPm7r5vGc2QmAj0INeRnL3lxi00syeIaA8W',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
