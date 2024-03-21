e90d0d17566c5e1383e0526e45eb6e6785466f3d
YasmineBB <y.bourhabi@gmail.com>
2024-03-21 21:34:56 +0000

    Update settings imports

--- curated/settings.py
+++ curated/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+from os import path
 
 from pathlib import Path
+if path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-odx_g&8)2x%g1bgq64ur(3$mfbd4b2513u3mpd+gs^@kt2pe#k'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
