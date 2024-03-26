e3da2e2264aaf7027132cfee3d1575c568e2e0d9
YasmineBB <y.bourhabi@gmail.com>
2024-03-26 15:45:14 +0000

    Update settings and secret key

--- curated/settings.py
+++ curated/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import dj_database_url
-from os import path
+if os.path.isfile('env.py'):
+    import env
 
 # from pathlib import Path
 # if path.exists('env.py'):
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
