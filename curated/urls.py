7628a8eb60bb25fc2cb88006d11ad46f6cbfb214
YasmineBB <y.bourhabi@gmail.com>
2024-04-01 16:19:20 +0000

    Add contact urls

--- curated/urls.py
+++ curated/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'curated.views.handler404'
