d9fa99d5bf30284d6b4f32ee07f75fc28d15e765
YasmineBB <y.bourhabi@gmail.com>
2024-04-01 12:32:49 +0000

    Add custom 404 and 500 pages

--- curated/urls.py
+++ curated/urls.py
@@ -28,3 +28,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'curated.views.handler404'
