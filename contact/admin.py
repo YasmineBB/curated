d80ea44b6aedffcca92ffecfd27a0dceb6b9d001
YasmineBB <y.bourhabi@gmail.com>
2024-04-01 16:38:57 +0000

    Add checkout form and register in admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import ContactForm
 
-# Register your models here.
+
+class ContactFormAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'contact_name',
+        'contact_email',
+        'contact_message',
+        'date',
+    )
+
+    fields = (
+        'contact_email',
+        'date',
+        'contact_name',
+        'contact_subject',
+        'contact_message',
+        'have_replied',
+    )
+
+    list_display = (
+        'contact_email',
+        'contact_name',
+        'date',
+        'have_replied',
+    )
+
+    ordering = ('-date',)
+
+
+admin.site.register(ContactForm, ContactFormAdmin)
---
+++ contact/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import ContactForm
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactForm
+        fields = (
+            'contact_name',
+            'contact_email',
+            'contact_subject',
+            'contact_message',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders,
+        remove auto-generated labels,
+        set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'contact_name': 'Full Name',
+            'contact_email': 'Email Address',
+            'contact_subject': 'Subject',
+            'contact_message': 'Your Message',
+        }
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
