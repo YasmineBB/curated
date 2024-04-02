424b9f204a6502420543bce823ac015a86c82b64
YasmineBB <y.bourhabi@gmail.com>
2024-04-02 16:19:37 +0000

    Add view, url and template for admin adding artist to the database

--- artists/urls.py
+++ artists/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_artists, name='artists'),
-    path('<artist_id>', views.artist_detail, name='artist_detail'),
+    path('<int:artist_id>/', views.artist_detail, name='artist_detail'),
+    path('add/', views.add_artist, name='add_artist'),
 ]
--- artists/views.py
+++ artists/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from products.models import Artist, Product
+from .forms import ArtistForm
 
 # Create your views here.
 
@@ -25,3 +28,33 @@ def artist_detail(request, artist_id):
     }
 
     return render(request, 'artists/artist_detail.html', context)
+
+
+@login_required
+def add_artist(request):
+    """ Add an artist to the database """
+    if not request.user.is_superuser and request.user.is_anonymous:
+        messages.error(request, 'Ooops! Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    # else:
+    #     if request.user.is_anonymous:
+    #         messages.error(request, 'Ooops! Sorry, only store owners can do that.')
+    #     return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ArtistForm(request.POST, request.FILES)
+        if form.is_valid():
+            artist = form.save()
+            messages.success(request, 'Success! The artist was added to the database.')
+            return redirect(reverse('artists'))
+        else:
+            messages.error(request, 'Failed to add artist. Please ensure the form is valid.')
+    else:
+        form = ArtistForm()
+        
+    template = 'artists/add_artist.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
