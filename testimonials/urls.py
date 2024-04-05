d7a84ec740d43ef5e8b267cf4cb68abd95f744ea
YasmineBB <y.bourhabi@gmail.com>
2024-04-05 22:28:55 +0000

    Add functionality to add, edit and delete a testimonial from the database

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.testimonials, name='testimonials'),
     path('add/', views.add_testimonial, name='add_testimonial'),
+    path('edit/<int:testimonial_id>/', views.edit_testimonial, name='edit_testimonial'),
+    path('delete/<int:testimonial_id>/', views.delete_testimonial, name='delete_testimonial'),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -27,16 +27,58 @@ def add_testimonial(request):
         form = TestimonialForm(request.POST, request.FILES)
         if form.is_valid():
             testimonial = form.save()
-            messages.success(request, f'Success! You added a feature of {testimonial.buyer_name} to the database.')
+            messages.success(request, f'Success! You added a feature by {testimonial.buyer_name} to the database.')
             return redirect(reverse('testimonials'))
         else:
             messages.error(request, 'Failed to add feature. Please ensure the form is valid.')
     else:
         form = TestimonialForm()
         
-    template = 'testimonials/add_testimonial.html'
+    template = 'testimonials/add_testimonial.html' 
     context = {
         'form': form,
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_testimonial(request, testimonial_id):
+    """ A view to make changes to a feature/testimonial in the database """
+    if not request.user.is_superuser:
+        messages.error(request, 'Ooops! Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    testimonial = get_object_or_404(Testimonial, pk=testimonial_id)
+    if request.method == 'POST':
+        form = TestimonialForm(request.POST, request.FILES, instance=testimonial)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Success! You have updated the feature of {testimonial.buyer_name}.')
+            return redirect(reverse('testimonials'))
+        else:
+            messages.error(request, 'Failed to update feature. Please ensure the form is valid.')
+    else:
+        form = TestimonialForm(instance=testimonial)
+        messages.info(request, f'You are editing the feature of {testimonial.buyer_name}')
+
+    template = 'testimonials/edit_testimonial.html'
+    context = {
+        'form': form,
+        'testimonial': testimonial,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_testimonial(request, testimonial_id):
+    """ A view to delete feature/testimonial from the database """
+    if not request.user.is_superuser:
+        messages.error(request, 'Ooops! Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    testimonial = get_object_or_404(Testimonial, pk=testimonial_id)
+    testimonial.delete()
+    messages.success(request, f'You deleted the feature for {testimonial.buyer_name} from the database!')
+    return redirect(reverse('testimonials'))
