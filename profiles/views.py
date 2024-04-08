3a75afde4f11e10199cdd860814af06b9e4076d8
YasmineBB <y.bourhabi@gmail.com>
2024-04-08 12:28:08 +0000

    Update profile view docstring

--- profiles/views.py
+++ profiles/views.py
@@ -10,7 +10,7 @@ from checkout.models import Order
 
 @login_required
 def profile(request):
-    """ Display the user's profile. """
+    """ A view to display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
