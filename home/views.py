373e2a4eabd89d2fc5fc5e6888db7570c282b789
YasmineBB <y.bourhabi@gmail.com>
2024-04-09 12:52:05 +0000

    Fix issues in home dir python code after pep8 validation

--- home/views.py
+++ home/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from django.urls import path
-from . import views
 
-# Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
 
 def privacy(request):
     """
